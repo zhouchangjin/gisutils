e071c4e0efb92f6bbf539a3f779e1a4b90af484c
unknown <CREATE TABLE IF NOT EXISTS undo_log(>
2024-09-09 21:33:39 +0800

    i

--- src/main/java/com/iwhere/gisutil/reader/TiffUtils.java
+++ src/main/java/com/iwhere/gisutil/reader/TiffUtils.java
@@ -27,6 +27,148 @@ public class TiffUtils {
 
     public static final double scalefactor=1.0;
 
+    public static BufferedImage CalcSlop2Band(RenderedImage image, double cellsize){
+        Raster data=image.getData();
+        int width=data.getWidth();
+        int height=data.getHeight();
+        BufferedImage out=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++){
+                double[] pixel=new double[1];
+                double rgb[]=data.getPixel(j,i,pixel);
+                double[] pixelWindow=new double[9];
+                int[] pixelWindowWeight=new int[9]; //用来计算权重
+                pixelWindow[4]=rgb[0];
+                pixelWindowWeight[4]=0;
+                if(j>0 && i>0){  //左上角
+                    double rgb_ul[]=data.getPixel(j-1,i-1,pixel);
+                    pixelWindow[0]=rgb_ul[0];
+                    if(rgb_ul[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[0]=1;
+                    }else{
+                        pixelWindowWeight[0]=0;
+                    }
+                }else{
+                    //处于边缘位置不记录
+                    pixelWindow[0]=0;
+                    pixelWindowWeight[0]=0;
+                }
+                if(i>0){  //正上方
+                    double rgb_u[]=data.getPixel(j,i-1,pixel);
+                    pixelWindow[1]=rgb_u[0];
+                    if(rgb_u[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[1]=2; //上下左右四个格子权重为2
+                    }else{
+                        pixelWindowWeight[1]=0;
+                    }
+                }else{
+                    pixelWindow[1]=0;
+                    pixelWindowWeight[1]=0;
+                }
+                if(j<width-1 && i>0){ //右上方
+                    double rgb_ur[]=data.getPixel(j+1,i-1,pixel);
+                    pixelWindow[2]=rgb_ur[0];
+                    if(rgb_ur[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[2]=1; //上下左右四个格子权重为2
+                    }else{
+                        pixelWindowWeight[2]=0;
+                    }
+                }else {
+                    pixelWindow[2]=0;
+                    pixelWindowWeight[2]=0;
+                }
+
+                if(j>0){ //正西
+                    double rgb_l[]=data.getPixel(j-1,i,pixel);
+                    pixelWindow[3]=rgb_l[0];
+                    if(rgb_l[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[3]=2; //上下左右四个格子权重为2
+                    }else{
+                        pixelWindowWeight[3]=0;
+                    }
+                }else{
+                    pixelWindow[3]=0;
+                    pixelWindowWeight[3]=0;
+                }
+
+                if(j<width-1){ //正东
+                    double rgb_r[]=data.getPixel(j+1,i,pixel);
+                    pixelWindow[5]=rgb_r[0];
+                    if(rgb_r[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[5]=2; //上下左右四个格子权重为2
+                    }else{
+                        pixelWindowWeight[5]=0;
+                    }
+                }else{
+                    pixelWindow[5]=0;
+                    pixelWindowWeight[5]=0;
+                }
+
+                if(j>0 && i<height-1){ //左下
+                    double rgb_bl[]=data.getPixel(j-1,i+1,pixel);
+                    pixelWindow[6]=rgb_bl[0];
+                    if(rgb_bl[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[6]=1; //上下左右四个格子权重为2
+                    }else{
+                        pixelWindowWeight[6]=0;
+                    }
+                }else{
+                    pixelWindow[6]=0;
+                    pixelWindowWeight[6]=0;
+                }
+
+                if(i<height-1){ //正下
+                    double rgb_b[]=data.getPixel(j,i+1,pixel);
+                    pixelWindow[7]=rgb_b[0];
+                    if(rgb_b[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[7]=2; //上下左右四个格子权重为2
+                    }else{
+                        pixelWindowWeight[7]=0;
+                    }
+                }else{
+                    pixelWindow[7]=0;
+                    pixelWindowWeight[7]=0;
+                }
+
+                if(i<height-1 && j<width-1){
+                    double rgb_br[]=data.getPixel(j+1,i+1,pixel);
+                    pixelWindow[8]=rgb_br[0];
+                    if(rgb_br[0]>=-999){
+                        //tiff里的数据如果是nodata，值为-32767或其他的用户指定的较小负数，因为是dem所以较小的负数直接为无效数据
+                        pixelWindowWeight[8]=1; //上下左右四个格子权重为2
+                    }else{
+                        pixelWindowWeight[8]=0;
+                    }
+                }else{
+                    pixelWindow[8]=0;
+                    pixelWindowWeight[8]=0;
+                }
+
+                double slopeHorizontal=calcPixelWindowSlopeHorizontal(pixelWindow,pixelWindowWeight,cellsize);
+                double slopeAngle=calcPixelWindowSlopeMethod1(pixelWindow,pixelWindowWeight,cellsize);
+                double slopeVertical=calcPixelWindowSlopeVertical(pixelWindow,pixelWindowWeight,cellsize);
+                int hSlope=(int)Math.round(Math.toDegrees(slopeHorizontal)*10); //最大900;
+                int vSlope=(int)Math.round(Math.toDegrees(slopeVertical)*10); //最大900;
+                int total=vSlope*100+hSlope; //最大900900 小于INT.MAX
+                //int rValue=(int)Math.round(Math.toDegrees(slopeAngle)*scalefactor);
+                //int gValue=(int)Math.round(Math.toDegrees(slopeHorizontal)*scalefactor);
+                //int bValue=(int)Math.round(Math.toDegrees(slopeVertical)*scalefactor);
+                //out.getRaster().setPixel(j,i,new int[]{rValue,gValue,bValue});
+                out.setRGB(j,i,total);
+            }
+
+        }
+        return out;
+    }
+
     public static BufferedImage CalcSlop3Band(RenderedImage image, double cellsize){
 
         Raster data=image.getData();
@@ -207,12 +349,12 @@ public class TiffUtils {
                 +pixelWindow[2]*pixelWindowWeight[2];
         double hs=pixelWindow[6]*pixelWindowWeight[6]
                 +pixelWindow[7]*pixelWindowWeight[7]
-                +pixelWindow[8]+pixelWindowWeight[8];
+                +pixelWindow[8]*pixelWindowWeight[8];
 
         double availableCellCntSN=pixelWindowWeight[0]+pixelWindowWeight[1]+pixelWindowWeight[2]
                 +pixelWindowWeight[6]+pixelWindowWeight[7]+pixelWindowWeight[8];
         if(availableCellCntSN==0){
-            return -32767;
+            return 0;
         }
 
         double dy=(hs-hn)/(availableCellCntSN*cellsize);
@@ -234,7 +376,7 @@ public class TiffUtils {
         double availableCellCntWE=pixelWindowWeight[0]+pixelWindowWeight[3]+pixelWindowWeight[6]
                 +pixelWindowWeight[2]+pixelWindowWeight[5]+pixelWindowWeight[8];
         if(availableCellCntWE==0){
-            return -32767;
+            return 0;
         }
         double dx=(hw-he)/(availableCellCntWE*cellsize);  // dz=hw-he  dx=cellnum*cellsize
         double slope=Math.sqrt(dx*dx);  //坡度x
@@ -402,12 +544,12 @@ public class TiffUtils {
             System.out.println(posGrid.x+"==="+posGrid.y);
             System.out.println(world.getCoordinate()[0]+","+world.getCoordinate()[1]);
             System.out.println(world2.getCoordinate()[0]+","+world.getCoordinate()[1]);
-            BufferedImage outImage= CalcSlop3Band(image,30);
-            ImageIO.write(outImage,"PNG",new FileOutputStream("d:/out1.png"));
+            BufferedImage outImage= CalcSlop2Band(image,30);
+            ImageIO.write(outImage,"PNG",new FileOutputStream("d:/hainanslope2.png"));
 
             ReferencedEnvelope re=new ReferencedEnvelope(coverage.getEnvelope2D(),crs);
             GridCoverage2D gc = gcf.create("name", outImage, re);
-            GeoTiffWriter geoTiffWriter=new GeoTiffWriter(new File("d:/out.tif"));
+            GeoTiffWriter geoTiffWriter=new GeoTiffWriter(new File("d:/hainanslope2.tif"));
             geoTiffWriter.write(gc,null);
             geoTiffWriter.dispose();
 
